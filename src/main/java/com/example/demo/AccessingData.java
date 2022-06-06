9fbbfbaa26463a9d0af445293b098e71a0f05008
n6fab <n6fab@disroot.org>
2022-06-06 19:40:25 +0200

    add AccessingData

---
+++ src/main/java/com/example/demo/AccessingData.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingData {
+    public static void main(String[]args) {
+        SpringApplication.run(AccessingData.class, args);
+    }
+}
